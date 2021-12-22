5089edcc69626883dcda840087d5ddb9d8580b16
Sam Smith <samsmithnz@gmail.com>
2021-12-21 21:45:44 -0500

    Created .NET projects

---
+++ src/RepoAutomation.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepoAutomation.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
