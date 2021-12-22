aa6105525b329e159b61bbade6fc6a58e2e3c5bc
Sam Smith <samsmithnz@gmail.com>
2021-12-21 22:08:20 -0500

    Setup hello world

---
+++ src/RepoAutomation/Program.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+
+namespace RepoAutomation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world");
+        //Parse arguments
+        string workingDirectory = Environment.CurrentDirectory;
+        Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+        {
+            if (string.IsNullOrEmpty(o.Directory) == false)
+            {
+                workingDirectory = o.Directory;
+            }
+        });
+
+    }
+
+    public class Options
+    {
+        [Option('d', "directory", Required = false, HelpText = "set working directory")]
+        public string? Directory { get; set; }
+    }
+}
