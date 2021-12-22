305107eb7bef5e182b6b221ebc06ecc98f25c727
Sam Smith <samsmithnz@gmail.com>
2021-12-22 07:34:57 -0500

    Added Base API

---
+++ src/RepoAutomation/APIAccess/GitHubAPIAccess.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace RepoAutomation.APIAccess;
+
+public static class GitHubAPIAccess
+{
+
+    //Call the GitHub Rest API to get a JSON array of runs
+    public async static Task<Newtonsoft.Json.Linq.JArray> GetRepo(string clientId, string clientSecret, string owner, string repo)
+    {
+        Newtonsoft.Json.Linq.JArray list = null;
+        string url = $"https://api.github.com/repos/{owner}/{repo}";
+        string response = await GetGitHubMessage(url, clientId, clientSecret);
+        if (string.IsNullOrEmpty(response) == false)
+        {
+            dynamic jsonObj = JsonConvert.DeserializeObject(response);
+            list = jsonObj.workflow_runs;
+        }
+        return list;
+    }
+
+    private async static Task<string> GetGitHubMessage(string url, string clientId, string clientSecret)
+    {
+        Console.WriteLine($"Running GitHub url: {url}");
+        string responseBody = "";
+        if (!url.Contains("api.github.com"))
+        {
+            throw new Exception("api.github.com missing from URL");
+        }
+        using (HttpClient client = new())
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("DevOpsMetrics", "0.1"));
+            //If we use a id/secret, we significantly increase the rate from 60 requests an hour to 5000. https://developer.github.com/v3/#rate-limiting
+            if (string.IsNullOrEmpty(clientId) == false && string.IsNullOrEmpty(clientSecret) == false)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", clientId, clientSecret))));
+            }
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            {
+                //Throw a response exception
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    responseBody = await response.Content.ReadAsStringAsync();
+                    //Console.WriteLine(responseBody);
+                }
+            }
+        }
+        return responseBody;
+    }
+
+}
---
+++ src/RepoAutomation/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "This rule ruins read-ability, in my opinion.", Scope = "member", Target = "~M:RepoAutomation.APIAccess.GitHubAPIAccess.GetGitHubMessage(System.String,System.String,System.String)~System.Threading.Tasks.Task{System.String}")]
