8673ff52594eb80f5887f9abdb5325f3a8cb6f66
Sam Smith <samsmithnz@gmail.com>
2021-12-23 12:21:36 -0500

    UPdated command tests

--- src/RepoAutomation.Tests/CommandTests.cs
+++ src/RepoAutomation.Tests/CommandTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoAutomation.Tests.Helpers;
 
 namespace RepoAutomation.Tests;
 
@@ -22,65 +23,8 @@ public class CommandTests
         string expected = @".NET SDK (6.0.101)
 Usage: dotnet [runtime-options] [path-to-application] [arguments]
 
-Execute a .NET application.
-
-runtime-options:
-  --additionalprobingpath <path>   Path containing probing policy and assemblies to probe for.
-  --additional-deps <path>         Path to additional deps.json file.
-  --depsfile                       Path to <application>.deps.json file.
-  --fx-version <version>           Version of the installed Shared Framework to use to run the application.
-  --roll-forward <setting>         Roll forward to framework version  (LatestPatch, Minor, LatestMinor, Major, LatestMajor, Disable).
-  --runtimeconfig                  Path to <application>.runtimeconfig.json file.
-
-path-to-application:
-  The path to an application .dll file to execute.
-
-Usage: dotnet [sdk-options] [command] [command-options] [arguments]
-
-Execute a .NET SDK command.
-
-sdk-options:
-  -d|--diagnostics  Enable diagnostic output.
-  -h|--help         Show command line help.
-  --info            Display .NET information.
-  --list-runtimes   Display the installed runtimes.
-  --list-sdks       Display the installed SDKs.
-  --version         Display .NET SDK version in use.
-
-SDK commands:
-  add               Add a package or reference to a .NET project.
-  build             Build a .NET project.
-  build-server      Interact with servers started by a build.
-  clean             Clean build outputs of a .NET project.
-  format            Apply style preferences to a project or solution.
-  help              Show command line help.
-  list              List project references of a .NET project.
-  msbuild           Run Microsoft Build Engine (MSBuild) commands.
-  new               Create a new .NET project or file.
-  nuget             Provides additional NuGet commands.
-  pack              Create a NuGet package.
-  publish           Publish a .NET project for deployment.
-  remove            Remove a package or reference from a .NET project.
-  restore           Restore dependencies specified in a .NET project.
-  run               Build and run a .NET project output.
-  sdk               Manage .NET SDK installation.
-  sln               Modify Visual Studio solution files.
-  store             Store the specified assemblies in the runtime package store.
-  test              Run unit tests using the test runner specified in a .NET project.
-  tool              Install or manage tools that extend the .NET experience.
-  vstest            Run Microsoft Test Engine (VSTest) commands.
-  workload          Manage optional workloads.
-
-Additional commands from bundled tools:
-  dev-certs         Create and manage development certificates.
-  fsi               Start F# Interactive / execute F# scripts.
-  sql-cache         SQL Server cache command-line tools.
-  user-secrets      Manage development user secrets.
-  watch             Start a file watcher that runs a command when files change.
-
-Run 'dotnet [command] --help' for more information on a command.
-";
-        Assert.AreEqual(expected, result);
+Execute a .NET application.";
+        Assert.AreEqual(expected, Utility.TakeNLines(result, 4));
     }
 
     [TestMethod]
@@ -99,49 +43,8 @@ Run 'dotnet [command] --help' for more information on a command.
            [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
            [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
            [--super-prefix=<path>] [--config-env=<name>=<envvar>]
-           <command> [<args>]
-
-These are common Git commands used in various situations:
-
-start a working area (see also: git help tutorial)
-   clone             Clone a repository into a new directory
-   init              Create an empty Git repository or reinitialize an existing one
-
-work on the current change (see also: git help everyday)
-   add               Add file contents to the index
-   mv                Move or rename a file, a directory, or a symlink
-   restore           Restore working tree files
-   rm                Remove files from the working tree and from the index
-   sparse-checkout   Initialize and modify the sparse-checkout
-
-examine the history and state (see also: git help revisions)
-   bisect            Use binary search to find the commit that introduced a bug
-   diff              Show changes between commits, commit and working tree, etc
-   grep              Print lines matching a pattern
-   log               Show commit logs
-   show              Show various types of objects
-   status            Show the working tree status
-
-grow, mark and tweak your common history
-   branch            List, create, or delete branches
-   commit            Record changes to the repository
-   merge             Join two or more development histories together
-   rebase            Reapply commits on top of another base tip
-   reset             Reset current HEAD to the specified state
-   switch            Switch branches
-   tag               Create, list, delete or verify a tag object signed with GPG
-
-collaborate (see also: git help workflows)
-   fetch             Download objects and refs from another repository
-   pull              Fetch from and integrate with another repository or a local branch
-   push              Update remote refs along with associated objects
-
-'git help -a' and 'git help -g' list available subcommands and some
-concept guides. See 'git help <command>' or 'git help <concept>'
-to read about a specific subcommand or concept.
-See 'git help git' for an overview of the system.
-";
-        Assert.AreEqual(expected, result);
+           <command> [<args>]";
+        Assert.AreEqual(expected, Utility.TakeNLines(result,6));
     }
 
 }
---
+++ src/RepoAutomation.Tests/Helpers/Utility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+
+namespace RepoAutomation.Tests.Helpers
+{
+    public static class Utility
+    {
+        public static string TakeNLines(string text, int lines)
+        {
+            return string.Join(Environment.NewLine, text.Split(Environment.NewLine).Take(lines));
+        }
+    }
+}
