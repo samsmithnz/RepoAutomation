5a1b0792398faee4a58acd2caba1283cc86e9645
Sam Smith <samsmithnz@gmail.com>
2021-12-22 21:59:55 -0500

    Commented out unused response props

--- src/RepoAutomation/Models/BaseModel.cs
+++ src/RepoAutomation/Models/BaseModel.cs
@@ -3,8 +3,8 @@
     public class BaseModel
     {
         public string? RawJSON { get; set; }
-        public bool IsSuccessfulResponse { get; set; }
-        public int? StatusCode { get; set; }
-        public string? Status { get; set; }
+        //public bool IsSuccessfulResponse { get; set; }
+        //public int? StatusCode { get; set; }
+        //public string? Status { get; set; }
     }
 }
