0eb127bfbc7240e55b1a4d34d3a982b534919981
Sam Smith <samsmithnz@gmail.com>
2021-12-23 12:01:43 -0500

    Added basic dotnet commands

---
+++ src/RepoAutomation.Tests/CommandTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepoAutomation.Tests;
+
+[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[TestClass]
+[TestCategory("IntegrationTests")]
+public class CommandTests
+{
+
+    [TestMethod]
+    public void DotNetTest()
+    {
+        //Arrange
+        CommandLine command = new();
+
+        //Act
+        string result = command.RunDotNetCommand("");
+
+        //Assert
+        string expected = @".NET SDK (6.0.101)
+Usage: dotnet [runtime-options] [path-to-application] [arguments]
+
+Execute a .NET application.
+
+runtime-options:
+  --additionalprobingpath <path>   Path containing probing policy and assemblies to probe for.
+  --additional-deps <path>         Path to additional deps.json file.
+  --depsfile                       Path to <application>.deps.json file.
+  --fx-version <version>           Version of the installed Shared Framework to use to run the application.
+  --roll-forward <setting>         Roll forward to framework version  (LatestPatch, Minor, LatestMinor, Major, LatestMajor, Disable).
+  --runtimeconfig                  Path to <application>.runtimeconfig.json file.
+
+path-to-application:
+  The path to an application .dll file to execute.
+
+Usage: dotnet [sdk-options] [command] [command-options] [arguments]
+
+Execute a .NET SDK command.
+
+sdk-options:
+  -d|--diagnostics  Enable diagnostic output.
+  -h|--help         Show command line help.
+  --info            Display .NET information.
+  --list-runtimes   Display the installed runtimes.
+  --list-sdks       Display the installed SDKs.
+  --version         Display .NET SDK version in use.
+
+SDK commands:
+  add               Add a package or reference to a .NET project.
+  build             Build a .NET project.
+  build-server      Interact with servers started by a build.
+  clean             Clean build outputs of a .NET project.
+  format            Apply style preferences to a project or solution.
+  help              Show command line help.
+  list              List project references of a .NET project.
+  msbuild           Run Microsoft Build Engine (MSBuild) commands.
+  new               Create a new .NET project or file.
+  nuget             Provides additional NuGet commands.
+  pack              Create a NuGet package.
+  publish           Publish a .NET project for deployment.
+  remove            Remove a package or reference from a .NET project.
+  restore           Restore dependencies specified in a .NET project.
+  run               Build and run a .NET project output.
+  sdk               Manage .NET SDK installation.
+  sln               Modify Visual Studio solution files.
+  store             Store the specified assemblies in the runtime package store.
+  test              Run unit tests using the test runner specified in a .NET project.
+  tool              Install or manage tools that extend the .NET experience.
+  vstest            Run Microsoft Test Engine (VSTest) commands.
+  workload          Manage optional workloads.
+
+Additional commands from bundled tools:
+  dev-certs         Create and manage development certificates.
+  fsi               Start F# Interactive / execute F# scripts.
+  sql-cache         SQL Server cache command-line tools.
+  user-secrets      Manage development user secrets.
+  watch             Start a file watcher that runs a command when files change.
+
+Run 'dotnet [command] --help' for more information on a command.
+";
+        Assert.AreEqual(expected, result);
+    }
+
+}
---
+++ src/RepoAutomation/CommandLine.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace RepoAutomation
+{
+    public class CommandLine
+    {
+        public string RunDotNetCommand(string arguments)
+        {
+            var process = new Process();
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet.exe",
+                Arguments = "-h",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+            StringBuilder sb = new();
+            while (!process.StandardOutput.EndOfStream)
+            {
+                sb.Append(process.StandardOutput.ReadLine());
+                sb.Append(Environment.NewLine);
+            }
+            return sb.ToString();
+
+            //$ProjectName = "RepoAutomationTest"
+
+            //dir
+            //cd\
+            //cd \users\samsm\source\repos
+            //dir
+            //mkdir $ProjectName
+            //cd $ProjectName
+            //clone 
+            //mkdir src
+            //cd src
+            //dotnet new mstest -n "$ProjectName.Tests"
+            //dotnet new webapp -n "$ProjectName.Web"
+            //dotnet new sln --name "$ProjectName"
+            //dotnet sln add "$ProjectName.Tests"
+            //dotnet sln add "$ProjectName.Web"
+        }
+    }
+}
