a5a8a012d1384bb2a812e979b222eff8c73d6367
Sam Smith <samsmithnz@gmail.com>
2021-12-29 11:22:56 -0500

    Updated action for test project

---
+++ src/RepoAutomation.Tests/ActionGenerationTests.cs
@@ -0,0 +1,38 @@
+﻿using GitHubActionsDotNet.Models;
+using GitHubActionsDotNet.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoAutomation.Helpers;
+using RepoAutomation.Tests.Helpers;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RepoAutomation.Tests;
+
+[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[TestClass]
+[TestCategory("IntegrationTests")]
+public class ActionGenerationTests
+{
+
+    [TestMethod]
+    public void CreateEmptyAction()
+    {
+        //Arrange
+        string projectName = "TestProject";
+        bool includeTestProject = false;
+        bool includeClassLibraryProject = false;
+        bool includeWebProject = false;
+
+        //Act
+        string yaml = GitHubActionsAutomation.CreateActionYaml(projectName,
+            includeTestProject,
+            includeClassLibraryProject,
+            includeWebProject);
+
+        //Assert
+        string expected = @"";
+        Assert.AreEqual(expected, Utility.TrimNewLines(yaml));
+    }
+
+}
--- src/RepoAutomation/Helpers/GitHubActionsAutomation.cs
+++ src/RepoAutomation/Helpers/GitHubActionsAutomation.cs
@@ -14,14 +14,11 @@ namespace RepoAutomation.Helpers
         {
             StringBuilder log = new();
 
-            //Create the YAML
-            GitHubActionsRoot root = CreateAction(projectName,
+            //Create and Serialize to YAML
+            string yaml = CreateActionYaml(projectName,
                 includeTestProject,
                 includeClassLibraryProject,
                 includeWebProject);
-
-            //Serialize to YAML
-            string yaml = GitHubActionsSerialization.Serialize(root);
 
             //Save the YAML to a file
             if (Directory.Exists(workingDirectory) == false)
@@ -45,9 +42,9 @@ namespace RepoAutomation.Helpers
             return log.ToString();
         }
 
-        private static GitHubActionsRoot CreateAction(string projectName, 
-            bool includeTestProject, 
-            bool includeClassLibraryProject, 
+        public static string CreateActionYaml(string projectName,
+            bool includeTestProject,
+            bool includeClassLibraryProject,
             bool includeWebProject)
         {
             JobHelper jobHelper = new();
@@ -59,17 +56,29 @@ namespace RepoAutomation.Helpers
 echo ""Version: ${{ steps.gitversion.outputs.SemVer }}""
 echo ""CommitsSinceVersionSource: ${{ steps.gitversion.outputs.CommitsSinceVersionSource }}""";
 
-            int stepCount = 0;
-            Step[] buildSteps = new Step[] {
-            CommonStepHelper.AddCheckoutStep(null,null,"0"),
-            GitVersionStepHelper.AddGitVersionSetupStep(),
-            GitVersionStepHelper.AddGitVersionDetermineVersionStep(),
-            CommonStepHelper.AddScriptStep("Display GitVersion outputs", displayBuildGitVersionScript),
-            DotNetStepHelper.AddDotNetSetupStep("Setup .NET","6.x"),
-            DotNetStepHelper.AddDotNetTestStep(".NET test","src/"+projectName+".Tests/"+projectName+".Tests.csproj","Release",null,true),
-            DotNetStepHelper.AddDotNetPublishStep(".NET publish","src/"+projectName+"/"+projectName+".csproj","Release",null,"-p:Version='${{ steps.gitversion.outputs.SemVer }}'", true),
-            CommonStepHelper.AddUploadArtifactStep("Upload package back to GitHub","drop","src/"+projectName+"/bin/Release")
-        };
+            List<Step> steps = new()
+            {
+                CommonStepHelper.AddCheckoutStep(null, null, "0"),
+                GitVersionStepHelper.AddGitVersionSetupStep(),
+                GitVersionStepHelper.AddGitVersionDetermineVersionStep(),
+                CommonStepHelper.AddScriptStep("Display GitVersion outputs", displayBuildGitVersionScript),
+                DotNetStepHelper.AddDotNetSetupStep("Setup .NET", "6.x")
+            };
+            if (includeTestProject == true)
+            {
+                steps.Add( DotNetStepHelper.AddDotNetTestStep(".NET test", "src/" + projectName + ".Tests/" + projectName + ".Tests.csproj", "Release", null, true));
+            }
+            if (includeClassLibraryProject == true)
+            {
+                steps.Add(DotNetStepHelper.AddDotNetPublishStep(".NET publish", "src/" + projectName + "/" + projectName + ".csproj", "Release", null, "-p:Version='${{ steps.gitversion.outputs.SemVer }}'", true));
+            }
+            if (includeWebProject == true)
+            {
+                steps.Add(DotNetStepHelper.AddDotNetPublishStep(".NET publish", "src/" + projectName + "/" + projectName + ".csproj", "Release", null, "-p:Version='${{ steps.gitversion.outputs.SemVer }}'", true));
+            }
+            steps.Add(CommonStepHelper.AddUploadArtifactStep("Upload package back to GitHub", "drop", "src/" + projectName + "/bin/Release"));
+            Step[] buildSteps = steps.ToArray();
+ 
             root.jobs = new();
             Job buildJob = jobHelper.AddJob(
                 "Build job",
@@ -81,7 +90,8 @@ echo ""CommitsSinceVersionSource: ${{ steps.gitversion.outputs.CommitsSinceVersi
                 { "CommitsSinceVersionSource", "${{ steps.gitversion.outputs.CommitsSinceVersionSource }}" }
             };
             root.jobs.Add("build", buildJob);
-            return root;
+            string yaml = GitHubActionsSerialization.Serialize(root);
+            return yaml;
         }
 
     }
