706a43aeb0e18c8f4402d9b4c23a1d9e5e772a9f
Sam Smith <samsmithnz@gmail.com>
2021-12-30 08:06:01 -0500

    Fixed Dependabot tests with .NET sample

--- src/RepoAutomation.Tests/DependabotGenerationTests.cs
+++ src/RepoAutomation.Tests/DependabotGenerationTests.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RepoAutomation.Tests.Helpers;
 using System.Collections.Generic;
 using GitHubActionsDotNet.Serialization;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace RepoAutomation.Tests;
 
@@ -17,25 +19,23 @@ public class DependabotGenerationTests
     {
         //Arrange
         string workingDirectory = System.Environment.CurrentDirectory;
+        string? projectDirectory = Directory.GetParent(workingDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName;
+        projectDirectory += "\\dependabotSamples";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            projectDirectory = projectDirectory?.Replace("\\", "/");
+        }
 
         //Act
-        List<string> files = FileSearch.GetFilesForDirectory(workingDirectory);
-        string yaml = DependabotSerialization.Serialize(workingDirectory, files);
+        List<string> files = FileSearch.GetFilesForDirectory(projectDirectory);
+        string yaml = DependabotSerialization.Serialize(projectDirectory, files);
 
 
         //Assert
         string expected = @"version: 2
 updates:
 - package-ecosystem: nuget
-  directory: /RepoAutomation/src/RepoAutomation.Tests/
-- package-ecosystem: nuget
-  directory: /RepoAutomation/src/RepoAutomation.Web/
-- package-ecosystem: nuget
-  directory: /RepoAutomation/src/RepoAutomation/
-- package-ecosystem: nuget
-  directory: /RepoTestProject/src/RepoTestProject.Tests/
-- package-ecosystem: nuget
-  directory: /RepoTestProject/src/RepoTestProject.Web/
+  directory: /dotnet/
 - package-ecosystem: github-actions
   directory: /";
         Assert.AreEqual(expected, Utility.TrimNewLines(yaml));
@@ -46,10 +46,16 @@ updates:
     {
         //Arrange
         string workingDirectory = System.Environment.CurrentDirectory;
+        string? projectDirectory = Directory.GetParent(workingDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName;
+        projectDirectory += "\\dependabotSamples";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            projectDirectory = projectDirectory?.Replace("\\", "/");
+        }
 
         //Act
-        List<string> files = FileSearch.GetFilesForDirectory(workingDirectory);
-        string yaml = DependabotSerialization.Serialize(workingDirectory, files,
+        List<string> files = FileSearch.GetFilesForDirectory(projectDirectory);
+        string yaml = DependabotSerialization.Serialize(projectDirectory, files,
             "daily",
             "06:00",
             "America/New_York",
@@ -61,43 +67,7 @@ updates:
         string expected = @"version: 2
 updates:
 - package-ecosystem: nuget
-  directory: /RepoAutomation/src/RepoAutomation.Tests/
-  schedule:
-    interval: daily
-    time: 06:00
-    timezone: America/New_York
-  assignees:
-  - samsmithnz
-  open-pull-requests-limit: 20
-- package-ecosystem: nuget
-  directory: /RepoAutomation/src/RepoAutomation.Web/
-  schedule:
-    interval: daily
-    time: 06:00
-    timezone: America/New_York
-  assignees:
-  - samsmithnz
-  open-pull-requests-limit: 20
-- package-ecosystem: nuget
-  directory: /RepoAutomation/src/RepoAutomation/
-  schedule:
-    interval: daily
-    time: 06:00
-    timezone: America/New_York
-  assignees:
-  - samsmithnz
-  open-pull-requests-limit: 20
-- package-ecosystem: nuget
-  directory: /RepoTestProject/src/RepoTestProject.Tests/
-  schedule:
-    interval: daily
-    time: 06:00
-    timezone: America/New_York
-  assignees:
-  - samsmithnz
-  open-pull-requests-limit: 20
-- package-ecosystem: nuget
-  directory: /RepoTestProject/src/RepoTestProject.Web/
+  directory: /dotnet/
   schedule:
     interval: daily
     time: 06:00
