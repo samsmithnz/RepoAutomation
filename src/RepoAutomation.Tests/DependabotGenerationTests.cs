d8198075d22fdc49d838aba1da7f2a54e236b3f1
Sam Smith <samsmithnz@gmail.com>
2021-12-30 07:42:13 -0500

    Added full example

--- src/RepoAutomation.Tests/DependabotGenerationTests.cs
+++ src/RepoAutomation.Tests/DependabotGenerationTests.cs
@@ -1,8 +1,8 @@
 ﻿using GitHubActionsDotNet.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RepoAutomation.Helpers;
 using RepoAutomation.Tests.Helpers;
 using System.Collections.Generic;
+using GitHubActionsDotNet.Serialization;
 
 namespace RepoAutomation.Tests;
 
@@ -20,7 +20,7 @@ public class DependabotGenerationTests
 
         //Act
         List<string> files = FileSearch.GetFilesForDirectory(workingDirectory);
-        string yaml = GitHubActionsDotNet.Serialization.DependabotSerialization.Serialize(workingDirectory, files);
+        string yaml = DependabotSerialization.Serialize(workingDirectory, files);
 
 
         //Assert
@@ -49,11 +49,71 @@ updates:
 
         //Act
         List<string> files = FileSearch.GetFilesForDirectory(workingDirectory);
-        string yaml = GitHubActionsDotNet.Serialization.DependabotSerialization.Serialize(workingDirectory, files);
-
+        string yaml = DependabotSerialization.Serialize(workingDirectory, files,
+            "daily",
+            "06:00",
+            "America/New_York",
+            new() { "samsmithnz" },
+            20,
+            true);
 
         //Assert
-        string expected = @"";
+        string expected = @"version: 2
+updates:
+- package-ecosystem: nuget
+  directory: /RepoAutomation/src/RepoAutomation.Tests/
+  schedule:
+    interval: daily
+    time: 06:00
+    timezone: America/New_York
+  assignees:
+  - samsmithnz
+  open-pull-requests-limit: 20
+- package-ecosystem: nuget
+  directory: /RepoAutomation/src/RepoAutomation.Web/
+  schedule:
+    interval: daily
+    time: 06:00
+    timezone: America/New_York
+  assignees:
+  - samsmithnz
+  open-pull-requests-limit: 20
+- package-ecosystem: nuget
+  directory: /RepoAutomation/src/RepoAutomation/
+  schedule:
+    interval: daily
+    time: 06:00
+    timezone: America/New_York
+  assignees:
+  - samsmithnz
+  open-pull-requests-limit: 20
+- package-ecosystem: nuget
+  directory: /RepoTestProject/src/RepoTestProject.Tests/
+  schedule:
+    interval: daily
+    time: 06:00
+    timezone: America/New_York
+  assignees:
+  - samsmithnz
+  open-pull-requests-limit: 20
+- package-ecosystem: nuget
+  directory: /RepoTestProject/src/RepoTestProject.Web/
+  schedule:
+    interval: daily
+    time: 06:00
+    timezone: America/New_York
+  assignees:
+  - samsmithnz
+  open-pull-requests-limit: 20
+- package-ecosystem: github-actions
+  directory: /
+  schedule:
+    interval: daily
+    time: 06:00
+    timezone: America/New_York
+  assignees:
+  - samsmithnz
+  open-pull-requests-limit: 20";
         Assert.AreEqual(expected, Utility.TrimNewLines(yaml));
     }
 
