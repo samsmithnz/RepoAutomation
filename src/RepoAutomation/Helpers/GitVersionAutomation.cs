2ad5f3a117e5f9a09c1cba9cdf777948a67ce39d
Sam Smith <samsmithnz@gmail.com>
2021-12-30 21:14:11 -0500

    Fix to create gitversion.yml file

--- src/RepoAutomation/Helpers/GitVersionAutomation.cs
+++ src/RepoAutomation/Helpers/GitVersionAutomation.cs
@@ -5,12 +5,8 @@
         public static void AddGitVersionFile(string workingDirectory, string startingVersion = "0.1.0")
         {
             string gitVersionPath = workingDirectory + "\\GitVersion.yml";
-            if (File.Exists(gitVersionPath) == true)
-            {
-                string contents = File.ReadAllText(gitVersionPath);
-                contents += "next-version: " + startingVersion;
-                File.WriteAllText(gitVersionPath, contents);
-            }
+            string contents = "next-version: " + startingVersion;
+            File.WriteAllText(gitVersionPath, contents);
         }
     }
 }
