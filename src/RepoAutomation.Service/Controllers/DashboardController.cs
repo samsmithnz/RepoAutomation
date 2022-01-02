e5d51107e663040987bbe1792ac830c2ee75f740
Sam Smith <samsmithnz@gmail.com>
2022-01-02 14:24:45 -0500

    Added initial web and service projects and started to mock up a pack

---
+++ src/RepoAutomation.Service/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RepoAutomation.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardController : ControllerBase
+    {
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //private readonly ILogger<DashboardController> _logger;
+
+        //public DashboardController(ILogger<DashboardController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
