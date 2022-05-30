097a332a7060e5b0fe19af7763648eb5398298da
Sam Smith <samsmithnz@gmail.com>
2022-05-30 11:12:24 -0400

    Adding Pull Requests to API

---
+++ src/RepoAutomation.Core/Models/PullRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoAutomation.Core.Models
+{
+    public class PullRequest
+    {
+        public string? Title { get; set; }
+        public string? Labels { get; set; }
+        public bool? IsDependabotPR { get; set; }
+        public bool? Approved { get; set; }
+        public string Status { get; set; }
+    }
+}
