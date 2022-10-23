d14ee8abd8ff083192d97ce01c6b53bae16028fc
Sam Smith <samsmithnz@gmail.com>
2022-10-22 22:16:38 -0400

    Added some suppressions for over the top rules

--- src/RepoAutomation.Core/GlobalSuppressions.cs
+++ src/RepoAutomation.Core/GlobalSuppressions.cs
@@ -6,5 +6,6 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "lower case is correct casing for GitHub Actions", Scope = "namespaceanddescendants", Target = "~N:RepoAutomation.Core.Models")]
+[assembly: SuppressMessage("Style", "IDE0074:Use compound assignment", Justification = "<Pending>", Scope = "member", Target = "~M:RepoAutomation.Core.Helpers.CommandLine.RunCommand(System.String,System.String,System.String)~System.String")]
 //[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "This rule ruins read-ability, in my opinion.", Scope = "member", Target = "~M:RepoAutomation.Core.APIAccess.BaseAPIAccess.GetGitHubMessage(System.String,System.String,System.String)~System.Threading.Tasks.Task{System.String}")]
 //[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "This rule ruins read-ability, in my opinion.", Scope = "member", Target = "~M:RepoAutomation.Core.APIAccess.BaseAPIAccess.PostGitHubMessage(System.String,System.String,System.String,System.Net.Http.StringContent)~System.Threading.Tasks.Task{System.String}")]
