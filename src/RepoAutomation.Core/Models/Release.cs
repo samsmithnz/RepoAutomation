79575d6bd019d035db626b756e91bf2e19c417b3
Sam Smith <samsmithnz@gmail.com>
2022-10-23 09:15:34 -0400

    Added new properties for release

--- src/RepoAutomation.Core/Models/Release.cs
+++ src/RepoAutomation.Core/Models/Release.cs
@@ -1,12 +1,35 @@
-﻿namespace RepoAutomation.Core.Models
+﻿using System.Text;
+
+namespace RepoAutomation.Core.Models
 {
     public class Release : BaseModel
     {
         public string? id { get; set; }
         public string? name { get; set; }
         public string? tag_name { get; set; }
-        public DateTime? published_at { get; set;  }
+        public DateTime? published_at { get; set; }
+        public string? url { get; set; }
         public Asset[]? assets { get; set; }
+
+        public string Timing
+        {
+            get
+            {
+                if (published_at != null)
+                {
+                    StringBuilder sb = new();
+                    sb.Append("(");
+                    
+                    sb.Append(") ");
+
+                    return sb.ToString();
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
     }
 
     public class Asset
